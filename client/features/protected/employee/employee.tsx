9920ebde92b4149c516ab37edcb4dcf011a1293e
radjikinseptiawan <radjikins21@gmail.com>
2026-07-18 06:05:05 +0700

    update employee feature

--- client/features/protected/employee/employee.tsx
+++ client/features/protected/employee/employee.tsx
@@ -1,13 +1,14 @@
+import { Container } from "@mui/material";
 import EmployeeControllers from "./controllers/controllers";
 import EmployeeAddDialog from "./dialogs/e.add";
 import TableEmployee from "./table/table";
 
 export default function EmployeeFeatures() {
   return (
-    <div className="my-4 w-full">
+    <Container className="my-4 w-full">
       <EmployeeControllers />
       <TableEmployee />
       <EmployeeAddDialog />
-    </div>
+    </Container>
   );
 }
